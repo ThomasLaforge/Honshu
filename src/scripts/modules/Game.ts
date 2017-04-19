3b2269a4aa3237ff6e1337e882a8be3ceaff4da5
Thomas Laforge <thomas.laforge.38@gmail.com>
2017-04-19 11:16:26 +0200

    get counts: only init

--- src/scripts/modules/Game.ts
+++ src/scripts/modules/Game.ts
@@ -1,15 +1,82 @@
 // imports
     import { Player }          from './Player';
-    import { Deck }            from './Deck'
+    import { Deck }            from './Deck';
+    import { Timer }            from './Timer';
+    import { Bonus }            from './Bonus';
 // -------
 
 class Game {
 
 	private _players: Player[]; 
     private _deck: Deck;
+    private _timer: Timer;
+    private _bonus: Bonus;
 
-	constructor() {}
-	
+	constructor(bonus: Bonus = null) {
+
+    }
+
+    getCityCount(){
+        let cpt = 0;
+        return cpt;    
+    }
+
+    getForestCount(){
+        let cpt = 0;
+        return cpt;    
+    }
+    
+    getLakeCount(){
+        let cpt = 0;
+        return cpt;    
+    }
+
+    getManufacturingCount(){
+        let cpt = 0;
+        return cpt;    
+    }
+
+    getBonusCount(){
+        let cpt = 0;
+
+        if(this.bonus){
+
+        }
+
+        return cpt;        
+    }
+
+    getTotalCount(){
+        return this.getCityCount() + this.getForestCount() + this.getLakeCount() + this.getManufacturingCount() + this.getBonusCount();
+    }
+
+// Getters / Setters
+
+	public get players(): Player[] {
+		return this._players;
+	}
+	public set players(value: Player[]) {
+		this._players = value;
+	}
+	public get deck(): Deck {
+		return this._deck;
+	}
+	public set deck(value: Deck) {
+		this._deck = value;
+	}
+	public get timer(): Timer {
+		return this._timer;
+	}
+	public set timer(value: Timer) {
+		this._timer = value;
+	}
+	public get bonus(): Bonus {
+		return this._bonus;
+	}
+	public set bonus(value: Bonus) {
+		this._bonus = value;
+	}
+
 //------------------- 
     
 
