bcde540138e137032e65995f63590b24ad897b92
Thomas Laforge <thomas.laforge.38@gmail.com>
2017-05-09 23:30:31 +0200

    some fixes

--- src/scripts/Vue/honshuMap.ts
+++ src/scripts/Vue/honshuMap.ts
@@ -1,18 +1,14 @@
 import { ResourceType } from '../modules/Honshu'
 
 let template = `
-<div class="grid">
+<div class="grid" @dragover.prevent @drop="onDropCard" @dragenter="onDragEnterCard">
     <table class="grid-table">
         <tr v-for="row in map.map">
             <td v-for="tile in row" class="grid-box" :class="'tile-type-' + tile.constructor.name">
-                <div v-if="tile.resource" class="tile-resource" :class="'tile-resource-' + tileResourceName(tile)" />
+                <div v-if="tile.resource || tile.resource === 0" class="tile-resource" :class="'tile-resource-' + tileResourceName(tile)" />
             </td>
         </tr>
     </table>
-
-    {{map.getTotalCount()}}
-
-    <button @click="askExtraction">Extract</button>
 </div>
 `
 
@@ -29,11 +25,14 @@ export const honshuMap = {
     components : {
     },
     methods: {
-        askExtraction : function(){
-            this.$emit('extract');
-        },
         tileResourceName: function(tile:any){
             return ResourceType[tile.resource]
+        },
+        onDropCard(){
+            console.log('on drop card')
+        },
+        onDragEnterCard(){
+            console.log('on Drag Enter card')
         }
     }
 };
--- src/scripts/modules/Tile.ts
+++ src/scripts/modules/Tile.ts
@@ -5,3 +5,24 @@ export abstract class Tile {
     }
 
 }
+
+import { CityTile, FieldTile, ForestTile, LakeTile } from './BasicTile'
+import { ProductionTile } from './ProductionTile'
+import { ManufacturingTile } from './ManufacturingTile'
+import { TileInterface, TileType } from './Honshu'
+
+export class TileCreator {
+    constructor(){}
+
+    static create(tileDef: TileInterface): Tile {
+        switch (tileDef.type) {
+            case TileType.City          : return new CityTile();
+            case TileType.Lake          : return new LakeTile();
+            case TileType.Forest        : return new ForestTile();
+            case TileType.Field         : return new FieldTile();
+            case TileType.Manufacturing : return new ManufacturingTile(tileDef.resource, tileDef.points);
+            case TileType.Production    : return new ProductionTile(tileDef.resource);
+            default: console.log('tile type doesn\'t exist'); break;
+        }
+    }
+}
