e1a8aaf16960d9fdcb72603e312fd7b3bdeaae45
Thomas Laforge <thomas.laforge.38@gamil.com>
2017-06-15 15:35:09 +0200

    rotate card

--- src/scripts/modules/PlayableCard.ts
+++ src/scripts/modules/PlayableCard.ts
@@ -8,37 +8,23 @@ export class PlayableCard {
     
     private _tiles: Tile[][]
     private _value: number
+    private clockwise: boolean
 
     constructor(tiles: Tile[] | Tile[][] | CardFace, value: number){
         this.value = value;
+        this.clockwise = null;
         if(!(tiles instanceof CardFace) && !Array.isArray(tiles[0])){
             tiles = _.chunk(tiles, CARD_COL)
         }
         this.tiles = tiles instanceof CardFace ? tiles.tiles : <Tile[][]>tiles;
     }
 
-    rotate(){
-        let newTiles = _.cloneDeep(this.tiles);
-        let N = this.tiles[1].length ;
-        for (let x = 0; x < N / 2; x++){
-            for (let y = x; y < N-x-1; y++){
-                // store current cell in temp variable
-                let temp = newTiles[x][y];
-    
-                // move values from right to top
-                newTiles[x][y] = newTiles[y][N-1-x];
-    
-                // move values from bottom to right
-                newTiles[y][N-1-x] = newTiles[N-1-x][N-1-y];
-    
-                // move values from left to bottom
-                newTiles[N-1-x][N-1-y] = newTiles[N-1-y][x];
-    
-                // assign temp to left
-                newTiles[N-1-y][x] = temp;
-            }
+    rotate(clockwise = true){
+        let turnToDo = clockwise ? 1 : 3;
+        for(let i = 0; i < turnToDo; i++){
+            this.clockwise = this.clockwise && !clockwise ? clockwise : false
+            this.tiles = _.zip.apply(_, this.clockwise ? this.tiles : this.tiles.reverse());
         }
-        this.tiles = newTiles;
     }
 
     public get tiles(){
